bf0042530efdaacd8dde1db47c398e71d4405dec
rgoodwin0331 <95768052+rgoodwin0331@users.noreply.github.com>
2022-01-14 16:59:47 -0500

    Update main.tf

--- AzureARM/main.tf
+++ AzureARM/main.tf
@@ -23,45 +23,4 @@ resource "azurerm_resource_group_template_deployment" "arm" {
   deployment_mode = "Complete"
   template_body = file("template.json")
 
-  parameters =  {
-        "location" =  {
-            "value" =  "eastus2"
-        },
-        "extendedLocation" =  {
-            "value" =  {}
-        },
-        "virtualNetworkName" =  {
-            "value" =  "SampleCo-Internal"
-        },
-        "resourceGroup" =  {
-            "value" =  "SampleCoRG"
-        },
-        "addressSpaces" =  {
-            "value" =  [
-                "172.16.0.0/16"
-            ]
-        },
-        "ipv6Enabled" =  {
-            "value" =  false
-        },
-        "subnetCount" =  {
-            "value" =  1
-        },
-        "subnet0_name" =  {
-            "value" =  "Servers"
-        },
-        "subnet0_addressRange" =  {
-            "value" =  "172.16.1.0/24"
-        },
-        "ddosProtectionPlanEnabled" =  {
-            "value" =  false
-        },
-        "firewallEnabled" =  {
-            "value" =  false
-        },  
-        "bastionEnabled" =  {
-            "value" =  false
-        }
-    }
-  
-}
+  }
